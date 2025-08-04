bf73143a4d91bb7655563abedef5514abbdba211
ely kassab <elykassab24@gmail.com>
2025-08-04 15:34:19 +0200

    working iמ auth with google

--- foodvibe_mobile/lib/features/auth/data/auth_repository.dart
+++ foodvibe_mobile/lib/features/auth/data/auth_repository.dart
@@ -1,19 +1,28 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
 import '../../../services/supabase_service.dart';
 
 class AuthRepository {
   final SupabaseClient _client;
 
-   AuthRepository({SupabaseClient? client})
-      : _client = client ?? SupabaseManager.client;
+  AuthRepository({SupabaseClient? client})
+    : _client = client ?? SupabaseManager.client;
 
-  Future<void> signInWithEmail({required String email}) async{
-     await _client.auth.signInWithOtp(email: email);
+  Future<void> signInWithEmail({required String email}) async {
+    await _client.auth.signInWithOtp(email: email);
   }
 
   Future<void> signInWithGoogle() async {
+    final callBackUrl = dotenv.env['CALLBACK_URL'];
+
+    if (callBackUrl == null) {
+      throw Exception('CALLBACK_URL is not set');
+    }
+
     await _client.auth.signInWithOAuth(
-      OAuthProvider.google
+      OAuthProvider.google,
+      redirectTo: callBackUrl,
     );
   }
 
--- foodvibe_mobile/lib/main.dart
+++ foodvibe_mobile/lib/main.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  try {
-    await dotenv.load(fileName: ".env");
-  } catch (e) {
-    print("Error loading .env file: $e");
-  }
+  await dotenv.load(fileName: ".env");
+  
   await Supabase.initialize(
-    url: dotenv.env['SUPABASE_URL']!,
-    anonKey: dotenv.env['SUPABASE_KEY']!,
+    url: dotenv.env['SUPABASE_URL'] ?? "",
+    anonKey: dotenv.env['SUPABASE_KEY'] ?? "",
   );
   runApp(const ProviderScope(child: MyApp()));
 }
